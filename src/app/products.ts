140f5661d249b2492c34389ace0bcf11164a3873
Mohamed Alaa El-Din <moaalaa16@gmail.com>
2022-02-06 12:36:55 +0200

    initial commit
    	modified:   src/app/products.ts
    	package-lock.json

---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export interface Anime {
+    id: number;
+    title: string;
+    views: number;
+    description: string;
+}
+
+export const animeList: Anime[] = [
+    {
+        id: 1,
+        title: 'Black Clover',
+        views: 799,
+        description: 'A Very awesome anime to watch it has my favorite anime thing MAGIC :)'
+    },
+    {
+        id: 2,
+        title: 'Bleach',
+        views: 699,
+        description: 'The biggest anime in fights and epic ost'
+    },
+    {
+        id: 3,
+        title: 'One Piece',
+        views: 299,
+        description: 'Amazing anime with very detailed world and mysteries'
+    }
+];
